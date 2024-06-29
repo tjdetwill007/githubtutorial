3647d1e83140eb68e4e968b9283843ef215e4739
Deepak <tjdetwill007@gmail.com>
2024-06-29 16:01:41 +0530

    changes

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def root():
-    return ("<h1>Test hello</h1>")
+    return ("<h1>Final Deployment</h1>")
 
 if __name__=='__main__':
     app.run(host="0.0.0.0",debug=True)
